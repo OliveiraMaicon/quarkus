04917fd7d07c56a00ac996fe6c219bb879851684
Maicon Oliveira <>
2020-05-15 14:49:34 -0300

    init project

---
+++ src/main/java/br/com/oliveira/learning/quarkus/QuarkusRestApplication.java
@@ -0,0 +1,8 @@
+package br.com.oliveira.learning.quarkus;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/data")
+public class QuarkusRestApplication extends Application {
+}
